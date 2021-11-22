808f7a4f856c9534ac68f314b4f7a803a4f7e290
Leithen <leithen113@gmail.com>
2021-11-22 15:23:44 -0700

    Connected server to SQL database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 // Create express server
 const express = require('express');
 const PORT = process.env.PORT || 3001;
@@ -6,6 +8,20 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password:  '1qaz@WSX3edc$RFV',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 // Default response (Page Not Found)
 app.use((req, res) => {
