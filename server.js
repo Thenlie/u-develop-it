4b3a96e0d0158a98361ac89cbefc36397d037304
Leithen <leithen113@gmail.com>
2021-11-22 15:35:33 -0700

    Added GET DELETE and CREATE routes for candidates

--- server.js
+++ server.js
@@ -19,9 +19,37 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
+// GET all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// })
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, res) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(res);
+// });
+
+// CREATE a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, res) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(res);
+});
 
 // Default response (Page Not Found)
 app.use((req, res) => {
