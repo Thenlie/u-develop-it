3249eea267d5ff80786d82cb37fd36bae43e734f
Leithen <leithen113@gmail.com>
2021-11-22 16:44:41 -0700

    Created parties table

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,10 @@
-CREATE TABLE candidates(
+CREATE TABLE parties (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
