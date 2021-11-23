0f2104af2c4c16a3ebd784e4fd6ff94d641e6a80
Leithen <leithen113@gmail.com>
2021-11-23 12:27:20 -0700

    Created voter routes

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use((req, res) => {
 // Start the express server at DB connection
 db.connect(err => {
     if (err) throw err;
-    console.log('Database Connected.');
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
